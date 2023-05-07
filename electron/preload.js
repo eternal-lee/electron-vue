f486f9cb6d8ebd0d64ffeb0d5438ea0d19d8153d
LiQiang <1176952591@qq.com>
2023-05-07 17:40:50 +0800

    feat: 初始化

---
+++ electron/preload.js
@@ -0,0 +1,10 @@
+// preload.js
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
+})
